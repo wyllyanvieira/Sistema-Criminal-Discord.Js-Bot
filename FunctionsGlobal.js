5034a30ed4c1a8f6b08dad3be67e36c803dd57ee
Wyllyan Vieira <wyllyanvieira@hotmail.com>
2024-06-17 11:37:39 -0400

    Removendo as Funções de SetConfig e GetConfig

--- FunctionsGlobal.js
+++ FunctionsGlobal.js
@@ -1,7 +1,6 @@
 
 const sqlite3 = require("sqlite3").verbose();
 
-// Conectar ao banco de dados SQLite3
 const db = new sqlite3.Database('./database.db');
 
 db.serialize(() => {
@@ -30,38 +29,7 @@ function formatarTempo(segundos) {
   return `${horas}h ${minutos}m ${segundosRestantes.toFixed(0)}s`;
 }
 
-// Função para pegar o valor da configuração pelo tipo
-function getConfig(type) {
-  try {
-      const row = db.prepare('SELECT value FROM config WHERE type = ?').get(type);
-      if (row) {
-          return row.value;
-      } else {
-          return null;
-      }
-  } catch (err) {
-      console.error(err.message);
-      return null;
-  }
-}
 
-// Função para definir o valor da configuração pelo tipo
-function setConfig(type, value) {
-  try {
-      const row = db.prepare('SELECT id FROM config WHERE type = ?').get(type);
-      if (row) {
-          // Se existir, atualizar
-          db.prepare('UPDATE config SET value = ? WHERE type = ?').run(value, type);
-      } else {
-          // Se não existir, inserir
-          db.prepare('INSERT INTO config (type, value) VALUES (?, ?)').run(type, value);
-      }
-      return true;
-  } catch (err) {
-      console.error(err.message);
-      return false;
-  }
-}
 
 
 module.exports = { log , formatarTempo, getConfig, setConfig};
